f166df529533f22986b2b8b8fd1535a8a0307a67
bryanlin16899@outlook.com <bryanlin16899@outlook.com>
2021-03-19 17:14:10 +0800

    Update 2021.03.19

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import os
 import requests
 from twilio.rest import Client
 
 OWN_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 MY_API_KEY = "964fcfec612a63e1860c42f376fc1f90"
+MY_PHONENUMBER = "+886972988302"
 
 weather_params = {
-    "lat": 24.147736,
-    "lon": 120.673645,
+    "lat": -1.237927,
+    "lon": 116.852852,
     "appid": MY_API_KEY,
     "exclude": "current,minutely,daily"
 }
@@ -16,8 +18,25 @@ response = requests.get(OWN_Endpoint, params=weather_params)
 response.raise_for_status()
 data = response.json()
 weather_slice = data["hourly"][:12]
+
+will_rain = False
+
 for hour_data in weather_slice:
     condition_code = hour_data["weather"][0]["id"]
+    print(condition_code)
     if int(condition_code) < 700:
-        print("Bring an umbrella.")
-# print(data["hourly"][0]["weather"][0]["id"])
+        will_rain = True
+
+if will_rain:
+    account_sid = 'AC54eaf9f436c566420931df8dbbb038a8'
+    auth_token = '0d8456dcb3755e650a5a8e30dc22bd29'
+    client = Client(account_sid, auth_token)
+
+    message = client.messages \
+        .create(
+        body="It's going to rain today, Remember to bring an umbrella.",
+        from_="+13603287144",
+        to=MY_PHONENUMBER
+    )
+    print(message.sid)
+    print("GOGO")
